ba17f21e0c8e99cc1751470e947241f5ad13a4d6
Max Rottenkolber <max@mr.gy>
2015-11-26 17:11:17 +0100

    [l2fwd] Log stats.

--- examples/l2fwd/main.c
+++ examples/l2fwd/main.c
@@ -150,39 +150,22 @@ print_stats(void)
 	total_packets_tx = 0;
 	total_packets_rx = 0;
 
-	const char clr[] = { 27, '[', '2', 'J', '\0' };
-	const char topLeft[] = { 27, '[', '1', ';', '1', 'H','\0' };
-
-		/* Clear screen and move to top left */
-	printf("%s%s", clr, topLeft);
-
-	printf("\nPort statistics ====================================");
-
 	for (portid = 0; portid < RTE_MAX_ETHPORTS; portid++) {
 		/* skip disabled ports */
 		if ((l2fwd_enabled_port_mask & (1 << portid)) == 0)
 			continue;
-		printf("\nStatistics for port %u ------------------------------"
-			   "\nPackets sent: %24"PRIu64
-			   "\nPackets received: %20"PRIu64
-			   "\nPackets dropped: %21"PRIu64,
-			   portid,
-			   port_statistics[portid].tx,
-			   port_statistics[portid].rx,
-			   port_statistics[portid].dropped);
-
 		total_packets_dropped += port_statistics[portid].dropped;
 		total_packets_tx += port_statistics[portid].tx;
 		total_packets_rx += port_statistics[portid].rx;
 	}
-	printf("\nAggregate statistics ==============================="
-		   "\nTotal packets sent: %18"PRIu64
-		   "\nTotal packets received: %14"PRIu64
-		   "\nTotal packets dropped: %15"PRIu64,
+        RTE_LOG(INFO, L2FWD,
+		   "Sent: %18"PRIu64
+		   " / Received: %14"PRIu64
+		   " / Dropped: %15"PRIu64
+                   "\n",
 		   total_packets_tx,
 		   total_packets_rx,
 		   total_packets_dropped);
-	printf("\n====================================================\n");
 }
 
 /* Send the burst of packets on an output interface */
